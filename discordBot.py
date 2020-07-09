aa49e8c4a7d053cc4a6fdc140fde1c4edf550b42
NotMyLyfe <30644833+NotMyLyfe@users.noreply.github.com>
2020-07-09 11:03:11 -0400

    Reverting back to old commit

--- discordBot.py
+++ discordBot.py
@@ -22,45 +22,46 @@ async def on_ready():
 
 @client.event
 async def on_command_error(ctx, error):
+    print("Error has occured. Error message: %s" % error)
     if isinstance(error, commands.CommandNotFound):
         await ctx.author.send("The command you've entered is invalid. To find the list of commands, type `~help` in the Discord channel.")
+    await ctx.channel.purge(limit=1)
 
 @client.command()
 async def add(ctx, username):
-    await ctx.channel.purge(limit=1)
     cursor.execute("SELECT EXISTS(SELECT * FROM `discord` WHERE `discordID` = '%s') as ex;" % ctx.author.id)
-
     if cursor.fetchone()[0] == 0:
         minecraftData = requests.get(url = "https://playerdb.co/api/player/minecraft/%s" % username)
-        if minecraftData.status_code == 500:
-            await ctx.author.send("The Minecraft username you specified is invalid. Make sure you typed it right before retrying in the Discord channel.")
-            pass
-        uuid = minecraftData.json().get("data").get("player").get("id")
-        cursor.execute("SELECT EXISTS(SELECT * FROM `discord` WHERE `uuid` = '%s') as ex;" % uuid)
-        if cursor.fetchone()[0] == 0:
-            cursor.execute("SELECT EXISTS(SELECT * FROM `whitelist` where `uuid` = '%s') as ex;" % uuid)
+        if minecraftData.status_code == 200:
+            uuid = minecraftData.json().get("data").get("player").get("id")
+            cursor.execute("SELECT EXISTS(SELECT * FROM `discord` WHERE `uuid` = '%s') as ex;" % uuid)
             if cursor.fetchone()[0] == 0:
-                cursor.execute("INSERT INTO `whitelist` (`uuid`, `name`, `whitelisted`) VALUES ('%s', '%s', 'true');" % (uuid, username))
+                cursor.execute("SELECT EXISTS(SELECT * FROM `whitelist` where `uuid` = '%s') as ex;" % uuid)
+                if cursor.fetchone()[0] == 0:
+                    cursor.execute("INSERT INTO `whitelist` (`uuid`, `name`, `whitelisted`) VALUES ('%s', '%s', 'true');" % (uuid, username))
+                else:
+                    cursor.execute("UPDATE `whitelist` SET `whitelisted` = 'true' WHERE `uuid` = '%s';" % (uuid))
+                sqlConnection.commit()
+                cursor.execute("INSERT INTO `discord` (`discordID`, `uuid`) VALUES ('%s', '%s');" % (ctx.author.id, uuid))
+                sqlConnection.commit()
+                await ctx.author.send("You have successfully whitelisted your Minecraft account!")
             else:
-                cursor.execute("UPDATE `whitelist` SET `whitelisted` = 'true' WHERE `uuid` = '%s';" % (uuid))
-            sqlConnection.commit()
-            cursor.execute("INSERT INTO `discord` (`discordID`, `uuid`) VALUES ('%s', '%s');" % (ctx.author.id, uuid))
-            sqlConnection.commit()
-            await ctx.author.send("You have successfully whitelisted your Minecraft account!")
+                await ctx.author.send("The Minecraft username you've specified has already been linked to an account.")
         else:
-            await ctx.author.send("The Minecraft username you've specified has already been linked to an account.")
+            await ctx.author.send("The Minecraft username you specified is invalid. Make sure you typed it right before retrying in the Discord channel.")
     else:
         await ctx.author.send("You currently already have a Minecraft account linked to your Discord account. To remove that account, please type `~remove` in the Discord channel.")
+    await ctx.channel.purge(limit=1)
 
 @add.error
 async def clear_error(ctx, error):
-    await ctx.channel.purge(limit=1)
+    print("Error has occured. Error message: %s" % error)
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.author.send("You have forgotten to place the username argument. To add your Minecraft account to the whitelist, type `~add (username)` in the Discord channel.")
+    await ctx.channel.purge(limit=1)
 
 @client.command()
 async def remove(ctx):
-    await ctx.channel.purge(limit=1)
     cursor.execute("SELECT EXISTS(SELECT * FROM `discord` WHERE `discordID` = '%s') as ex;" % ctx.author.id)
     if cursor.fetchone()[0] == 0:
         await ctx.author.send("You currently don't have a Minecraft account linked to your Discord account.")
@@ -69,8 +70,9 @@ async def remove(ctx):
         uuid = cursor.fetchone()[2]
         cursor.execute("DELETE FROM `discord` WHERE `discordID` = '%s';" % ctx.author.id)
         sqlConnection.commit()
-        cursor.execute("UPDATE `whitelist` SET `whitelisted` = 'false' where `uuid` = '%s';" % uuid);
+        cursor.execute("UPDATE `whitelist` SET `whitelisted` = 'false' where `uuid` = '%s';" % uuid)
         sqlConnection.commit()
         await ctx.author.send("You have successfully unlinked your Minecraft account.")
+    await ctx.channel.purge(limit=1)
 
 client.run(os.getenv("discordToken"))
